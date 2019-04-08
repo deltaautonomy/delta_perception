9263f29f1e7669c1b3c081ad862490ecd74eb372
Heethesh Vhavle <heethesh@gmail.com>
2019-04-08 01:20:06 -0400

    Vehicle ground thruth message + docs complete

---
+++ test/README.md
@@ -0,0 +1,86 @@
+# Test Scripts
+
+This folder contains some unit tests and sample scripts.
+
+## 1. Test for VehicleGroundTruthArray ROS message
+
+### Setup
+
+The message is defined as follows. It contains the ground truth data for all visible vehilces present in the camera's view.
+
+#### VehicleGroundTruthArray.msg
+
+```
+Header header
+VehicleGroundTruth[] vehicles
+```
+
+#### VehicleGroundTruth.msg
+
+```
+string class_name
+uint16 left
+uint16 top
+uint16 right
+uint16 bottom
+float64 distance
+bool difficult
+```
+
+#### Descriptions
+
+- std_msgs/string - class name
+- std_msgs/uint16 - 2D bbox corners (range: 0 - 65535)
+- std_msgs/float64 - radial distance (meters) of vehicle from ego vehicle
+- std_msgs/bool - difficult flag, `True`, if vehicle distance > 50m (**subject to change**)
+
+### Sample Usage
+
+Run the following commands to test the message publisher.
+
+```
+catkin_make
+source devel/setup.bash
+rosrun delta_perception vehicle_message_publisher
+```
+
+You should be able to echo the `/delta/ground_truth/vehicles` topic. A sample message output is shown below.
+
+```
+rostopic echo /delta/ground_truth/vehicles
+```
+
+---
+header: 
+  seq: 13
+  stamp: 
+    secs: 1554700067
+    nsecs:  65536975
+  frame_id: ''
+vehicles: 
+  - 
+    class_name: "car"
+    left: 506
+    top: 445
+    right: 737
+    bottom: 340
+    distance: 51.068106185
+    difficult: True
+  - 
+    class_name: "motorbike"
+    left: 190
+    top: 563
+    right: 655
+    bottom: 682
+    distance: 119.03513482
+    difficult: True
+  - 
+    class_name: "bicycle"
+    left: 493
+    top: 247
+    right: 159
+    bottom: 683
+    distance: 18.3572980964
+    difficult: False
+---
+```
---
+++ test/vehicle_message_publisher.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Author  : Heethesh Vhavle
+Email   : heethesh@cmu.edu
+Version : 1.0.0
+Date    : Apr 08, 2019
+'''
+
+import random
+import rospy
+from delta_perception.msg import VehicleGroundTruth, VehicleGroundTruthArray
+
+random_vehicles = ['car', 'truck', 'bus', 'motorbike', 'bicycle']
+
+def talker():
+    # Setup node
+    pub = rospy.Publisher('/delta/ground_truth/vehicles', VehicleGroundTruthArray, queue_size=10)
+    rospy.init_node('vehicle_publisher', anonymous=True)
+    
+    # Ignore this if you use a camera/image callback to publish the data
+    r = rospy.Rate(0.25)
+
+    # Randomly publish some data
+    while not rospy.is_shutdown():
+        # Create the message array
+        msg = VehicleGroundTruthArray()
+
+        # Create few random vehicles
+        for i in range(random.randrange(5)):
+            # Populate single vehicle with random ground truth data
+            vehicle = VehicleGroundTruth()
+
+            # std_msgs/string - class name
+            vehicle.class_name = random.choice(random_vehicles)
+            # std_msgs/uint16 - 2D bbox corners (range: 0 - 65535)
+            vehicle.left = random.randint(0, 1000)
+            vehicle.top = random.randint(0, 1000)
+            vehicle.right = random.randint(0, 1000)
+            vehicle.bottom = random.randint(0, 1000)
+            # std_msgs/float64 - radial distance (m) of vehicle from ego vehicle
+            vehicle.distance = random.random() * 150
+            # std_msgs/bool - difficult flag, True, if vehicle distance > 50m
+            vehicle.difficult = vehicle.distance > 50
+
+            # Add the vehicle to the message array
+            msg.vehicles.append(vehicle)
+
+        # Header stamp and publish the message
+        print('Sending')
+        msg.header.stamp = rospy.Time.now()
+        pub.publish(msg)
+
+        # Ignore this if you use a camera/image callback to publish the data
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass

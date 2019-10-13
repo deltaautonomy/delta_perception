b129fc570bd7e0e307a4632ddd8dd49360776e27
Heethesh Vhavle <heethesh@gmail.com>
2019-10-13 05:47:37 -0400

    Added icon and trajectory msg generators

--- scripts/cube_marker_publisher.py
+++ scripts/cube_marker_publisher.py
@@ -15,7 +15,9 @@ from __future__ import print_function, absolute_import, division
 import rospy
 
 # ROS messages
+from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
+from jsk_rviz_plugins.msg import Pictogram
 
 
 ########################### Functions ###########################
@@ -39,13 +41,14 @@ def make_label(text, position, frame_id='/map', marker_id=0,
     """
     marker = Marker()
     marker.header.frame_id = frame_id
+    marker.header.stamp = rospy.Time.now()
     marker.id = marker_id
     marker.type = marker.TEXT_VIEW_FACING
     marker.text = text
     marker.action = marker.ADD
-    marker.scale.x = 1.0
-    marker.scale.y = 1.0
-    marker.scale.z = 1.0
+    marker.scale.x = 1.5
+    marker.scale.y = 1.5
+    marker.scale.z = 1.5
     marker.color.a = 1.0
     marker.color.r = color[0]
     marker.color.g = color[1]
@@ -55,6 +58,80 @@ def make_label(text, position, frame_id='/map', marker_id=0,
     marker.pose.position.x = position[0]
     marker.pose.position.y = position[1]
     marker.pose.position.z = position[2]
+    return marker
+
+
+def make_pictogram(character, position, frame_id='/map',
+    duration=0.5, color=[1.0, 1.0, 1.0]):
+    """ 
+    Helper function for generating visualization markers
+    
+    Args:
+        character (str): Character (icon) to be displayed.
+        position (list): List containing [x,y,z] positions
+        frame_id (str): ROS TF frame id
+        duration (rospy.Duration): How long the label will be displayed for
+        color (list): List of label color floats from 0 to 1 [r,g,b]
+    
+    Returns: 
+        Pictogram: A jsk_rviz_plugins/Pictogram message which can be published to RViz
+    """
+    msg = Pictogram()
+    msg.action = Pictogram.ADD
+    msg.header.frame_id = frame_id
+    msg.header.stamp = rospy.Time.now()
+    msg.mode = Pictogram.PICTOGRAM_MODE
+    msg.character = character
+    msg.speed = 1.0
+    msg.ttl = duration
+    msg.size = 3
+    msg.color.r = color[0]
+    msg.color.g = color[1]
+    msg.color.b = color[2]
+    msg.color.a = 1.0
+    msg.pose.orientation.x = 0.0
+    msg.pose.orientation.y = -1.0
+    msg.pose.orientation.z = 0.0
+    msg.pose.orientation.w = 1.0
+    msg.pose.position.x = position[0]
+    msg.pose.position.y = position[1]
+    msg.pose.position.z = position[2]
+    return msg
+
+
+def make_trajectory(trajectory, frame_id='/map', marker_id=0,
+    duration=0.5, color=[1.0, 1.0, 1.0]):
+    """ 
+    Helper function for generating visualization markers
+    
+    Args:
+        trajectory (array-like): (n, 2) array-like trajectory data
+        frame_id (str): ROS TF frame id
+        marker_id (int): Integer identifying the trajectory
+        duration (rospy.Duration): How long the trajectory will be displayed for
+        color (list): List of color floats from 0 to 1 [r,g,b]
+    
+    Returns: 
+        Marker: A trajectory marker message which can be published to RViz
+    """
+    marker = Marker()
+    marker.header.stamp = rospy.Time.now()
+    marker.header.frame_id = frame_id
+    marker.id = marker_id
+    marker.type = marker.LINE_STRIP
+    marker.action = marker.ADD
+    for x, y in trajectory:
+        point = Point()
+        point.x = x
+        point.y = y
+        point.z = 0.5
+        marker.points.append(point)    
+    marker.scale.x = 0.15
+    marker.color.r = color[0]
+    marker.color.g = color[1]
+    marker.color.b = color[2]
+    marker.color.a = 1.0
+    marker.lifetime = rospy.Duration(duration)
     return marker
 
 
@@ -83,10 +160,10 @@ def make_cuboid(position, scale, frame_id='/map', marker_id=0,
     marker.scale.x = scale[0]
     marker.scale.y = scale[1]
     marker.scale.z = scale[2]
-    marker.color.a = 1.0
     marker.color.r = color[0]
     marker.color.g = color[1]
     marker.color.b = color[2]
+    marker.color.a = 1.0
     marker.lifetime = rospy.Duration(duration)
     marker.pose.orientation.w = 1.0
     marker.pose.position.x = position[0]

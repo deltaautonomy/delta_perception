4a8d76672b65e85174fc42caccf57288febae578
Heethesh Vhavle <heethesh@gmail.com>
2019-11-25 12:05:55 -0500

    Lane validation completed

---
+++ erfnet/lane_validator.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Author  : Heethesh Vhavle
+Email   : heethesh@cmu.edu
+Version : 1.0.0
+Date    : Nov 25, 2019
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function, absolute_import, division
+
+# Standalone
+if __name__ == '__main__':
+    import sys
+    sys.path.append('..')
+
+    # Handle paths and OpenCV import
+    from scripts.init_paths import *
+
+# Run from scripts module
+else:
+    # Handle paths and OpenCV import
+    from init_paths import *
+
+# External modules
+import matplotlib.pyplot as plt
+from natsort import natsorted
+from scipy.spatial import distance
+from scipy.optimize import linear_sum_assignment
+from pyclustering.cluster.kmedians import kmedians
+
+# Local python modules
+from ipm.ipm import InversePerspectiveMapping
+from erfnet.lane_filter import LaneKalmanFilter
+
+LANE_GT_COLOR = np.array([50, 230, 155])
+
+
+class LaneValidator:
+    def __init__(self):
+        self.ipm = InversePerspectiveMapping()
+        self.lane_filter = LaneKalmanFilter()
+        self.first_time = True
+        self.kmedians_centroids = np.asarray([[0, 30], [0, 90], [0, 130]], dtype=np.float64)
+
+    def hough_line_detector(self, ipm_lane):
+        canny_lane = cv2.Canny(ipm_lane, 50, 200, None, 3)
+
+        # Copy edges to the images that will display the results in BGR.
+        dst = cv2.cvtColor(canny_lane, cv2.COLOR_GRAY2BGR)
+
+        # Probabilistic hough lines detector.
+        lines_lanes = cv2.HoughLinesP(canny_lane, 1, np.pi / 180, 50, None, 50, 10)
+        if lines_lanes is not None:
+            lines_lanes = lines_lanes.squeeze(1)
+            for i in range(0, len(lines_lanes)):
+                l = lines_lanes[i]
+                cv2.line(dst, (l[0], l[1]), (l[2], l[3]), (0, 255, 255), 1, cv2.LINE_AA)
+
+        # Handle no line detections.
+        return dst, lines_lanes
+
+    def associate_lanes(self, medians, pred_medians):
+        association = [None] * 3
+        cost = distance.cdist(np.expand_dims(pred_medians[:, 1], 1), np.expand_dims(medians[:, 1], 1))
+        idx_a, idx_b = linear_sum_assignment(cost)
+        for i, j in zip(idx_a, idx_b): association[i] = medians[j]
+        return association
+
+    def detect_lines(self, img, timestamp):
+        # Rescale to camera image size (720 * 1280)
+        img_gt = cv2.resize(img, (0, 0), fx=1.6, fy=1.6)
+        img_gt = img_gt[120:840, :, :]
+        ipm_img = self.ipm.transform_image(img_gt)
+        output = ipm_img
+
+        # Segment out lanes
+        image1 = np.where(img_gt >= LANE_GT_COLOR - 10, 255, 0)
+        image2 = np.where(img_gt <= LANE_GT_COLOR + 10, 255, 0)
+        img_gt = (image1 & image2).astype('uint8')
+        img_gt = self.ipm.transform_image(img_gt)
+
+        # Make the lines thick
+        kernel = np.ones((2, 2), np.uint8)
+        img_gt_lane = cv2.dilate(img_gt, kernel, iterations=2)
+        ret, thresh = cv2.threshold(cv2.cvtColor(img_gt_lane, cv2.COLOR_BGR2GRAY), 10, 255, 0)
+        dst, lines = self.hough_line_detector(img_gt_lane)
+
+        # Compute slope and intercept of all lines.
+        slopes = (lines[:, 2] - lines[:, 0]) / (lines[:, 3] - lines[:, 1])
+        intercepts = np.mean(np.c_[lines[:, 0], lines[:, 2]], axis=1)
+        data = np.c_[slopes, intercepts]
+
+        # Create instance of K-Medians algorithm.
+        initial_medians = self.kmedians_centroids
+        initial_medians[:, 0] = [np.mean(slopes)] * 3
+        kmedians_instance = kmedians(data, initial_medians)
+
+        # Run cluster analysis and obtain results.
+        kmedians_instance.process()
+        clusters = kmedians_instance.get_clusters()
+        medians = np.asarray(kmedians_instance.get_medians())
+        median_slope = np.median(medians[:, 0])
+
+        # Kalman filter initialization.
+        if self.first_time:
+            if len(medians) == 3:
+                self.first_time = False
+                self.lane_filter.initialize_filter(timestamp.to_sec(), medians)
+            else:
+                sys.stdout.write("\r\033[31mInitialization requires medians for all lanes\t" )
+                sys.stdout.flush()
+                return None, None
+
+        # Kalman filter predict/update.
+        pred_medians = self.lane_filter.predict_step(timestamp.to_sec())
+        pred_medians = pred_medians.reshape(6, 2)[:, 0].reshape(3, 2)
+        lanes = self.associate_lanes(medians, pred_medians)
+        if not self.first_time:
+            medians = self.lane_filter.update_step(lanes[0], lanes[1], lanes[2])
+            medians = medians.reshape(6, 2)[:, 0].reshape(3, 2)
+
+        # Visualize lines on image.
+        colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
+        points_ipm = []
+        for (slope, intercept), color in zip(medians, colors):
+            x = np.array([0, ipm_img.shape[0] - 1]).astype('int')
+            y = ((median_slope * x) + intercept).astype('int')
+            points_ipm.append(np.c_[y, x])
+            cv2.line(output, (y[0], x[0]), (y[1], x[1]), color, 3, cv2.LINE_AA)
+        points_ipm = np.vstack(points_ipm)
+
+        # Convert data to meters.
+        points_m = self.ipm.transform_points_to_m(points_ipm, transform=False)
+        points_m = points_m.reshape(3, 4)
+        slopes_m = (points_m[:, 2] - points_m[:, 0]) / (points_m[:, 3] - points_m[:, 1])
+        lane_data_m = np.c_[slopes_m, points_m[:, 0]]
+
+        # Convert points from IPM to image coordinates.
+        points_img = self.ipm.transform_points_to_px(points_ipm, inverse=True)
+        points_img = points_img.reshape(3, 4)
+        points_ipm = points_ipm.reshape(3, 4)
+
+        return output, lane_data_m
+
+    @staticmethod
+    def slope_intercept_to_points(lanes):
+        x = np.linspace(0, 100, 3)
+        y1 = lanes[0, 0] * x + lanes[0, 1]
+        y2 = lanes[1, 0] * x + lanes[1, 1]
+        y3 = lanes[2, 0] * x + lanes[2, 1]
+        y = np.r_[y1, y2, y3]
+        points = np.r_[np.c_[x, y1], np.c_[x, y2], np.c_[x, y3]]
+        return points
+
+
+def load_files(folder):
+    files = natsorted([file for file in os.listdir(folder) if file.endswith('.jpg')])
+    return files
+
+
+if __name__ == '__main__':
+    files = load_files('gt_seg')
+    validator = LaneValidator()
+    
+    for file in files:
+        img = cv2.imread('gt_seg/' + file)
+        output, lanes = validator.detect_lines(img, time.time())
+        points = validator.slope_intercept_to_points(lanes)
+        plt.plot(points[0:3, 0], points[0:3, 1], '-k', linewidth=3.0, label='1')
+        plt.plot(points[3:6, 0], points[3:6, 1], '-k', linewidth=3.0, label='2')
+        plt.plot(points[6:9, 0], points[6:9, 1], '-k', linewidth=3.0, label='3')
+        plt.show()
+        # cv2.imshow('Output', output)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        break
+
--- scripts/lane_validation.py
+++ scripts/lane_validation.py
@@ -4,8 +4,8 @@
 '''
 Author  : Heethesh Vhavle
 Email   : heethesh@cmu.edu
-Version : 1.0.0
-Date    : Apr 07, 2019
+Version : 1.0.1
+Date    : Nov 25, 2019
 
 References:
 http://wiki.ros.org/message_filters
@@ -19,7 +19,6 @@ from __future__ import print_function, absolute_import, division
 
 # Handle paths and OpenCV import
 from init_paths import *
-from utils import *
 
 # External modules
 import matplotlib.pyplot as plt
@@ -34,24 +33,28 @@ from sensor_msgs.msg import Image
 from sklearn.cluster import DBSCAN
 
 # Local python modules
-from lanenet.lanenet import LaneNetModel
+from utils import *
+from erfnet.lane_validator import LaneValidator
+from erfnet.lane_detection import ERFNetLaneDetector
+from ipm.ipm import InversePerspectiveMapping
 
 # Global objects
 cmap = plt.get_cmap('tab10')
 LANE_GT_COLOR = np.array([50, 230, 155])
 PRED_COUNTER = 0
 GT_COUNTER = 0
-DELTA_FAR = 0.04 * 1280
-DELTA_NEAR = 0.06 * 1280
 
-# Perception models
-lanenet = LaneNetModel()
+# Classes
+lane_validator = LaneValidator()
+lane_detector = ERFNetLaneDetector()
+ipm = InversePerspectiveMapping()
+lane_fps = FPSLogger('LaneNet')
 
 
 ########################### Functions ###########################
 
 
-def perception_callback(image_msg, image_gt_msg, image_pub, **kwargs):
+def callback(image_msg, image_gt_msg, image_pub, **kwargs):
     # Read image message
     global GT_COUNTER, PRED_COUNTER
 
@@ -59,101 +62,39 @@ def perception_callback(image_msg, image_gt_msg, image_pub, **kwargs):
     img = message_to_cv2(image_msg)
     img_gt = message_to_cv2(image_gt_msg)
 
-    # Rescale to camera image size (720 * 1280)
-    img_gt = cv2.resize(img_gt, (0, 0), fx=1.6, fy=1.6)
-    img_gt = img_gt[120:840, :, :]
+    # Get slope intercepts of lanes
+    output, lanes_det = lane_detector.run(img, rospy.Time.now())
+    output, lanes_gt = validator.detect_lines(img, rospy.Time.now())
 
-    # Segment out lanes
-    image1 = np.where(img_gt >= LANE_GT_COLOR - 10, 255, 0)
-    image2 = np.where(img_gt <= LANE_GT_COLOR + 10, 255, 0)
-    img_gt = (image1 & image2).astype('uint8')
+    # Convert to points
+    points_det = validator.slope_intercept_to_points(lanes_det)
+    points_gt = validator.slope_intercept_to_points(lanes_gt)
 
-    # Make the lines thick
-    kernel = np.ones((7, 7), np.uint8)
-    img_gt_lane = cv2.dilate(img_gt, kernel, iterations=3)
-    ret, thresh = cv2.threshold(cv2.cvtColor(img_gt_lane, cv2.COLOR_BGR2GRAY), 127, 255, 0)
+    # Computer error
+    PRED_COUNTER += np.sum(np.int64(np.abs(points_det - points_gt)[:, 1] < 0.5))
+    GT_COUNTER += 9
 
-    # Find contours
-    thresh[:200] = 0
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-    # Keep only max 3 contours
-    args = np.argsort([cv2.contourArea(cnt) for cnt in contours])[::-1]
-    if len(args) < 3:
-        print('Less than 3 lanes in ground truth visible')
-        return
-    contours = [contours[arg] for arg in args[:3]]
-
-    # Sort them left to right
-    args = np.argsort([int(cv2.moments(cnt)['m10']/cv2.moments(cnt)['m00']) for cnt in contours])
-    contours = [contours[arg] for arg in args[:3]]
-    
-    # Run lane detection
-    lane_img, _, points = lanenet.run(img)
-    if len(points) < 3:
-        print('Less than 3 lanes in detected lanes visible')
-        return
-
-    # Fit lines on contours
-    rows, cols = img_gt_lane.shape[:2]
-    fy = img.shape[0] / float(lane_img.shape[0])
-    fx = img.shape[1] / float(lane_img.shape[1])
-    
-    gt_points = []
-    for point, cnt in zip(points, contours):
-        [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
-
-        # Validation line 1
-        gtx = int((((point[1] * fy) - y) * vx/vy) + x)
-        gt_points.append([gtx, int(point[1] * fy)])
-        cv2.circle(img, (int(gtx), int(point[1] * fy)), 5, (0, 255, 0), -1)
-
-        # Validation line 2
-        gtx = int((((point[3] * fy) - y) * vx/vy) + x)
-        gt_points.append([gtx, int(point[3] * fy)])
-        cv2.circle(img, (int(gtx), int(point[3] * fy)), 5, (0, 255, 0), -1)
-
-    # Sort by top and bottom
-    args = [1, 3, 5, 0, 2, 4]
-    gt_points = np.asarray(gt_points)[args]
-
-    # Scale predicted points
-    points[:, 0] = points[:, 0] * fx
-    points[:, 1] = points[:, 1] * fy
-    points[:, 2] = points[:, 2] * fx
-    points[:, 3] = points[:, 3] * fy
-    points = points.reshape(-1, 2)[args]
-
-    # Compute detections
-    errors = np.abs(points[:, 0] - gt_points[:, 0])
-    GT_COUNTER += 3
-    if errors[0] < DELTA_FAR and errors[3] < DELTA_NEAR: PRED_COUNTER += 1
-    if errors[1] < DELTA_FAR and errors[4] < DELTA_NEAR: PRED_COUNTER += 1
-    if errors[2] < DELTA_FAR and errors[5] < DELTA_NEAR: PRED_COUNTER += 1
-
-    # Display the detections
-    for point in points: cv2.circle(img, tuple(point.tolist()), 5, (0, 0, 255), -1)
-    cv2_to_message(img, image_pub)
+    cv2_to_message(img_gt, image_pub)
 
 
 def shutdown_hook():
     print('\n\033[95m' + '*' * 30 + ' Lane Validation Shutdown ' + '*' * 30 + '\033[00m\n')
-    lanenet.close()
-    print('Lane Detection Accuracy: %.2f%%\n' % (PRED_COUNTER * 100.0 / GT_COUNTER))
+    lane_detector.close()
+    # print('Lane Detection Accuracy: %.2f%%\n' % (PRED_COUNTER * 100.0 / GT_COUNTER))
 
 
 def run(**kwargs):
     # Start node
-    rospy.init_node('main', anonymous=True)
+    rospy.init_node('lane_validation', anonymous=True)
     rospy.loginfo('Current PID: [%d]' % os.getpid())
     
     # Setup models
-    lanenet.setup()
+    lane_detector.setup()
 
     # Handle params and topics
     image_color = rospy.get_param('~image_color', '/carla/ego_vehicle/camera/rgb/front/image_color')
     image_gt = rospy.get_param('~image_gt', '/carla/ego_vehicle/camera/semantic_segmentation/front/image_segmentation')
-    output_image = rospy.get_param('~output_image', '/delta/perception/lane_validation')
+    output_image = rospy.get_param('~output_image', '/delta/perception/lane_validation/image')
 
     # Publish output topic
     image_pub = rospy.Publisher(output_image, Image, queue_size=5)
@@ -165,7 +106,7 @@ def run(**kwargs):
     # Synchronize the topics by time
     ats = message_filters.ApproximateTimeSynchronizer(
         [image_sub, image_gt_sub], queue_size=1, slop=0.5)
-    ats.registerCallback(perception_callback, image_pub, **kwargs)
+    ats.registerCallback(callback, image_pub, **kwargs)
 
     # Shutdown hook
     rospy.on_shutdown(shutdown_hook)
